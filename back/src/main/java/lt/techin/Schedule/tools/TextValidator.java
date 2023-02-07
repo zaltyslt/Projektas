d4a8248e96dfbd3572bfd80d0ed2a40448255ed7
SauliusMon <88854073+SauliusMon@users.noreply.github.com>
2023-02-07 19:41:53 +0200

    Removing non-static checker

--- back/src/main/java/lt/techin/Schedule/tools/TextValidator.java
+++ back/src/main/java/lt/techin/Schedule/tools/TextValidator.java
@@ -14,7 +14,7 @@ public class TextValidator implements ConstraintValidator<TextValid, CharSequenc
     @Override
     public void initialize(TextValid constraintAnnotation) {
         textMaximumLength = constraintAnnotation.textMaximumLength();
-        rebuildPattern();
+        buildPattern();
     }
 
     @Override
@@ -27,27 +27,16 @@ public class TextValidator implements ConstraintValidator<TextValid, CharSequenc
         }
     }
 
-    //In a case Annotation check isn't enough
-    public boolean isTextValid (CharSequence textToCheck, int textMaximumLength) {
-        this.textMaximumLength = textMaximumLength;
-        rebuildPattern();
-        Matcher matcher = validSymbolsPattern.matcher(textToCheck);
-        try {
-            return matcher.matches();
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
     /* The pattern matches strings that start with 1 to (int textMaximumLength) characters,
     consisting of letters (uppercase or lowercase), digits, commas, dashes, dots or spaces.
     */
-    private void rebuildPattern () {
+    private void buildPattern () {
         this.validSymbolsPattern = Pattern.compile("^[A-Za-z0-9-,.'\\s]{1," + textMaximumLength + "}$");
     }
 
-    //In a case new object creation should be avoided
-    public static boolean isTextValidStatic (CharSequence textToCheck, int textMaximumLength) {
+
+    //In a case Annotation check isn't enough
+    public static boolean isTextValid (CharSequence textToCheck, int textMaximumLength) {
         Pattern validSymbolsPattern = Pattern.compile("^[A-Za-z0-9-,.'\\s]{1," + textMaximumLength + "}$");
         Matcher matcher = validSymbolsPattern.matcher(textToCheck);
         try {
--- back/src/test/java/lt/techin/Schedule/TextValidatorTests.java
+++ back/src/test/java/lt/techin/Schedule/TextValidatorTests.java
@@ -8,28 +8,21 @@ public class TextValidatorTests {
 
     @Test
     public void testValidator() {
-        TextValidator textValidator = new TextValidator();
+        Assertions.assertTrue(TextValidator.isTextValid("IShouldBeValid", 50), "Simple text containing letters should be valid");
+        Assertions.assertTrue(TextValidator.isTextValid("I Should Be Valid", 50), "Simple text containing letters and spaces should be valid");
+        Assertions.assertTrue(TextValidator.isTextValid("I'm0123456789Should.Still,Be-Valid", 50), "Text containing letters, spaces, digits, commas, dashes, and dots should be valid");
 
-        Assertions.assertTrue(textValidator.isTextValid("IShouldBeValid", 50), "Simple text containing letters should be valid");
-        Assertions.assertTrue(textValidator.isTextValid("I Should Be Valid", 50), "Simple text containing letters and spaces should be valid");
-        Assertions.assertTrue(textValidator.isTextValid("I'm0123456789Should.Still,Be-Valid", 50), "Text containing letters, spaces, digits, commas, dashes, and dots should be valid");
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Symbols %", 50), "Symbol % should be illegal");
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Symbols $", 50), "Symbol $ should be illegal");
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Symbols =", 50), "Symbol = should be illegal");
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Symbols +", 50), "Symbol + should be illegal");
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Symbols *", 50), "Symbol * should be illegal");
 
-        Assertions.assertFalse(textValidator.isTextValid("I Should Have Illegal Symbols %", 50), "Symbol % should be illegal");
-        Assertions.assertFalse(textValidator.isTextValid("I Should Have Illegal Symbols $", 50), "Symbol $ should be illegal");
-        Assertions.assertFalse(textValidator.isTextValid("I Should Have Illegal Symbols =", 50), "Symbol = should be illegal");
-        Assertions.assertFalse(textValidator.isTextValid("I Should Have Illegal Symbols +", 50), "Symbol + should be illegal");
-        Assertions.assertFalse(textValidator.isTextValid("I Should Have Illegal Symbols *", 50), "Symbol * should be illegal");
-
-        Assertions.assertFalse(textValidator.isTextValid("This Text Is 51 Symbols Long12012345678901234567890", 50), "Text shouldn't exceed 50 symbols");
-        Assertions.assertFalse(textValidator.isTextValid("I Should Have Illegal Length Because This Text WIll Exceed 50 Symbols And Will Be Really Really Long " +
+        Assertions.assertFalse(TextValidator.isTextValid("This Text Is 51 Symbols Long12012345678901234567890", 50), "Text shouldn't exceed 50 symbols");
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Length Because This Text WIll Exceed 50 Symbols And Will Be Really Really Long " +
                 "So Long That You Should Never Read Me Until The End", 50), "Text shouldn't exceed 50 symbols");
 
-        Assertions.assertFalse(textValidator.isTextValid("This", 2), "Text shouldn't exceed the length method received");
-        Assertions.assertFalse(textValidator.isTextValid("This Text Is Too Long For Test To Work", 25), "Text shouldn't exceed the length method received");
-
-        Assertions.assertTrue(TextValidator.isTextValidStatic("I'm0123456789Should.Still,Be-Valid", 50), "Text containing letters, spaces, digits, commas, dashes, and dots should be valid in a static method");
-        Assertions.assertFalse(TextValidator.isTextValidStatic("I Should Have Illegal Symbols %", 50), "Symbol % should be illegal in a static method");
-        Assertions.assertFalse(textValidator.isTextValid("This Text Is 51 Symbols Long12012345678901234567890", 50), "Text shouldn't exceed 50 symbols in a static method");
-        Assertions.assertFalse(textValidator.isTextValid("This", 2), "Text shouldn't exceed the length method received in a static method");
+        Assertions.assertFalse(TextValidator.isTextValid("This", 2), "Text shouldn't exceed the length method received");
+        Assertions.assertFalse(TextValidator.isTextValid("This Text Is Too Long For Test To Work", 25), "Text shouldn't exceed the length method received");
     }
 }
