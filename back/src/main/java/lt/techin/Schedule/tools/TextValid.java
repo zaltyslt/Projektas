293ac005c6eda94ce3a941e90cfbb99aebda337d
SauliusMon <88854073+SauliusMon@users.noreply.github.com>
2023-02-07 21:22:15 +0200

    Kom1 183 (#2)
    * Adding TextValid validator and respective JUnit Tests
    
    * Update TextValidatorTests.java
    
    * Update TextValidator.java
    
    * Annotation @TextValid maximum length value added, making class non-static
    
    * Removing non-static checker
    
    * Update TextValidator.java

---
+++ back/src/main/java/lt/techin/Schedule/tools/TextValid.java
@@ -0,0 +1,26 @@
+package lt.techin.Schedule.tools;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ FIELD })
+@Retention(RUNTIME)
+@Constraint(validatedBy = TextValidator.class)
+@Documented
+public @interface TextValid {
+
+    String message() default "{Text.invalid}";
+
+    int textMaximumLength() default 50;
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
---
+++ back/src/test/java/lt/techin/Schedule/TextValidatorTests.java
@@ -0,0 +1,28 @@
+package lt.techin.Schedule;
+
+import lt.techin.Schedule.tools.TextValidator;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class TextValidatorTests {
+
+    @Test
+    public void testValidator() {
+        Assertions.assertTrue(TextValidator.isTextValid("IShouldBeValid", 50), "Simple text containing letters should be valid");
+        Assertions.assertTrue(TextValidator.isTextValid("I Should Be Valid", 50), "Simple text containing letters and spaces should be valid");
+        Assertions.assertTrue(TextValidator.isTextValid("I'm0123456789Should.Still,Be-Valid", 50), "Text containing letters, spaces, digits, commas, dashes, and dots should be valid");
+
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Symbols %", 50), "Symbol % should be illegal");
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Symbols $", 50), "Symbol $ should be illegal");
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Symbols =", 50), "Symbol = should be illegal");
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Symbols +", 50), "Symbol + should be illegal");
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Symbols *", 50), "Symbol * should be illegal");
+
+        Assertions.assertFalse(TextValidator.isTextValid("This Text Is 51 Symbols Long12012345678901234567890", 50), "Text shouldn't exceed 50 symbols");
+        Assertions.assertFalse(TextValidator.isTextValid("I Should Have Illegal Length Because This Text WIll Exceed 50 Symbols And Will Be Really Really Long " +
+                "So Long That You Should Never Read Me Until The End", 50), "Text shouldn't exceed 50 symbols");
+
+        Assertions.assertFalse(TextValidator.isTextValid("This", 2), "Text shouldn't exceed the length method received");
+        Assertions.assertFalse(TextValidator.isTextValid("This Text Is Too Long For Test To Work", 25), "Text shouldn't exceed the length method received");
+    }
+}
