c68d09d5487d9c1ee8bcba11b27cdd114b24eb90
Egle Zilenaite <zilenaite.egle@gmail.com>
2023-02-08 17:07:50 +0200

    Add listpage table frontend(no pagination yet)

--- front/schedule-maker/src/pages/subjects/SubjectList.js
+++ front/schedule-maker/src/pages/subjects/SubjectList.js
@@ -1,7 +1,37 @@
+import { MDBTable, MDBTableHead, MDBTableBody, MDBPagination } from "mdb-react-ui-kit";
+import { useEffect, useState } from "react";
+
 export function SubjectList() {
-    return (
-        <div>
-            Sample subject.
-        </div>
-    );
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/v1/subjects")
+      .then((response) => response.json())
+      .then((jsonResponse) => setSubjects(jsonResponse));
+  }, []);
+
+  return (
+    <div>
+      <h3>Dalykų sąrašas</h3>
+      <MDBTable hover>
+        <MDBTableHead className='table-primary'>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Dalyko pavadinimas</th>
+            <th scope="col">Modulio pavadinimas</th>
+          </tr>
+        </MDBTableHead>
+        <MDBTableBody>
+            {subjects.map((subject, index) => (
+                <tr key={subject.id}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{subject.name}</td>
+                    <td>{subject.module}</td>
+                </tr>
+            ))}
+        </MDBTableBody>
+      </MDBTable>
+      <MDBPagination />
+    </div>
+  );
 }
