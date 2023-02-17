69f7e56abfc0d773ef671863298e3b75f65391b5
sandra jonikaite <sandrajonikaite@gmail.com>
2023-02-17 13:24:17 +0200

    not working ModuleServiceTest

--- back/src/test/java/lt/techin/schedule/module/ModuleServiceTest.java
+++ back/src/test/java/lt/techin/schedule/module/ModuleServiceTest.java
@@ -1,105 +1,46 @@
 package lt.techin.schedule.module;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+//@ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class ModuleServiceTest {
 
-//    import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertNotNull;
-//import static org.junit.Assert.assertTrue;
+//    @Mock
+//    ModuleRepository moduleRepository;
 //
-//import java.util.List;
-//import java.util.Optional;
+//    @InjectMocks
+//    private ModuleService service;
 //
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Sort;
+//    @Mock
+//    private EntityManager entityManager;
 //
-//import lt.techin.schedule.module.Module;
-//import lt.techin.schedule.module.ModuleRepository;
+//    @BeforeEach
+//    public void setUp() {
+//        service = new ModuleService(moduleRepository, entityManager);
+//    }
+//
+//    @Test
+//    public void testFindAllEmployees()
+//    {
+//        List<Module> list = new ArrayList<Module>();
+//        Module empOne = new Module(1L,"Steve", "Waugh", false );
+//        Module empTwo = new Module(2L,"Steve1", "Waugh1", true );
+//        Module empThree = new Module(3L,"Steve2", "Waugh3", false );
+//
+//        list.add(empOne);
+//        list.add(empTwo);
+//        list.add(empThree);
+//
+//        when(moduleRepository.findAll()).thenReturn(list);
+//
+//        //test
+//        List<Module> result = service.getAll(false);
+//
+//        assertEquals("",3, result.size());
+//
+//        verify(moduleRepository, times(1)).findAll();
+//    }
 
-//        private ModuleRepository moduleRepository = Mockito.mock(ModuleRepository.class);
-//
-//        private ModuleService moduleService;
-//
-//        @BeforeEach
-//        void setUp() {
-//            moduleService = new ModuleService(moduleRepository);
-//        }
-//
-//        @Test
-//        void testGetAll() {
-//            Mockito.when(moduleRepository.findAll()).thenReturn(List.of(new Module(1L, "Module 1", "Module", true), new Module("Module 2"));
-//            List<Module> modules = moduleService.getAll(false);
-//            assertEquals(2, modules.size());
-//        }
-//
-//        @Test
-//        void testGetById() {
-//            Mockito.when(moduleRepository.findById(1L)).thenReturn(Optional.of(new Module("Module 1")));
-//            Optional<Module> module = moduleService.getById(1L);
-//            assertTrue(module.isPresent());
-//            assertEquals("Module 1", module.get().getName());
-//        }
-//
-//        @Test
-//        void testCreate() {
-//            Module module = new Module(null, "Modulekod 1", "Modulepav", LocalDateTime.now(), LocalDateTime.now(), true);
-//            Mockito.when(moduleRepository.save(module)).thenReturn( new Module(1L, "Modulekod 1", "Modulepav", LocalDateTime.now(), LocalDateTime.now(), true));
-//            Module savedModule = moduleService.create(module);
-//            assertNotNull(savedModule);
-//            assertEquals("Module 1", savedModule.getName());
-//        }
-//
-//        @Test
-//        void testUpdateModule() {
-//            Module existingModule = new Module("Module 1");
-//            existingModule.setId(1L);
-//            Module updatedModule = new Module("Module 2");
-//            updatedModule.setId(1L);
-//
-//            Mockito.when(moduleRepository.findById(1L)).thenReturn(Optional.of(existingModule));
-//            Mockito.when(moduleRepository.save(existingModule)).thenReturn(updatedModule);
-//
-//            Module savedModule = moduleService.updateModule(1L, updatedModule);
-//            assertNotNull(savedModule);
-//            assertEquals("Module 2", savedModule.getName());
-//        }
-//
-//        @Test
-//        void testRestoreModule() {
-//            Module module = new Module("Module 1");
-//            module.setId(1L);
-//            module.setDeleted(true);
-//
-//            Mockito.when(moduleRepository.findById(1L)).thenReturn(Optional.of(module));
-//            Mockito.when(moduleRepository.save(module)).thenReturn(module);
-//
-//            Module restoredModule = moduleService.restoreModule(1L);
-//            assertNotNull(restoredModule);
-//            assertFalse(restoredModule.isDeleted());
-//        }
-//
-//        @Test
-//        void testDeleteById() {
-//            Mockito.doNothing().when(moduleRepository).deleteById(1L);
-//            boolean deleted = moduleService.deleteById(1L);
-//            assertTrue(deleted);
-//
-//            Mockito.doThrow(EmptyResultDataAccessException.class).when(moduleRepository).deleteById(2L);
-//            boolean notDeleted = moduleService.deleteById(2L);
-//            assertFalse(notDeleted);
-//        }
-//
-//        @Test
-//        void testFindAllPaged() {
-//            Mockito.when(moduleRepository.findAll(PageRequest.of(0, 2, Sort.Direction.DESC, "name"))).thenReturn(Page.empty());
-//            Page<Module> modules = moduleService.findAllPaged(0, 2, false);
-//            assertNotNull(modules);
-//        }
-
-    }
-
-
-
+}
