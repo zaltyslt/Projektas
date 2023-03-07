c8519245c37883073b826f334f891301facc34c8
Karolina Čižaitė <61018680+kcizaite@users.noreply.github.com>
2023-03-07 15:35:23 +0200

    Create schedule

---
+++ back/src/main/java/lt/techin/schedule/schedules/ScheduleMapper.java
@@ -0,0 +1,31 @@
+package lt.techin.schedule.schedules;
+
+public class ScheduleMapper {
+    public static ScheduleDto toScheduleDto(Schedule schedule) {
+        var scheduleDto = new ScheduleDto();
+        scheduleDto.setId(schedule.getId());
+//        scheduleDto.setGroups(schedule.getGroups());
+        scheduleDto.setSchoolYear(schedule.getSchoolYear());
+        scheduleDto.setSemester(schedule.getSemester());
+        scheduleDto.setDateFrom(schedule.getDateFrom());
+        scheduleDto.setDateUntil(schedule.getDateUntil());
+        scheduleDto.setActive(schedule.isActive());
+        scheduleDto.setCreatedDate(schedule.getCreatedDate());
+        scheduleDto.setModifiedDate(schedule.getModifiedDate());
+        return scheduleDto;
+    }
+
+    public static Schedule toSchedule(ScheduleDto scheduleDto) {
+        var schedule = new Schedule();
+        schedule.setId(scheduleDto.getId());
+//        schedule.setGroups(scheduleDto.getGroups());
+        schedule.setSchoolYear(scheduleDto.getSchoolYear());
+        schedule.setSemester(scheduleDto.getSemester());
+        schedule.setDateFrom(scheduleDto.getDateFrom());
+        schedule.setDateUntil(scheduleDto.getDateUntil());
+        schedule.setActive(scheduleDto.isActive());
+        schedule.setCreatedDate(scheduleDto.getCreatedDate());
+        schedule.setModifiedDate(scheduleDto.getModifiedDate());
+        return schedule;
+    }
+}
---
+++ back/src/main/java/lt/techin/schedule/schedules/ScheduleService.java
@@ -0,0 +1,40 @@
+package lt.techin.schedule.schedules;
+
+import lt.techin.schedule.group.GroupRepository;
+import lt.techin.schedule.group.GroupService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+    private final GroupRepository groupRepository;
+
+    public ScheduleService(ScheduleRepository scheduleRepository,
+                           GroupRepository groupRepository,
+                           GroupService groupService) {
+        this.scheduleRepository = scheduleRepository;
+        this.groupRepository = groupRepository;
+    }
+
+    public List<Schedule> getAll() {
+        return scheduleRepository.findAll();
+    }
+
+    public Schedule findById(Long id) {
+        return scheduleRepository.findById(id).orElse(null);
+    }
+
+    public Schedule create(Schedule schedule) {
+//        if (schedule.getGroups() == null) return null;
+//        {
+            try {
+                return scheduleRepository.save(schedule);
+            } catch (Exception e) {
+                System.out.println(e.getLocalizedMessage());
+            }
+            return null;
+        }
+//    }
+}
