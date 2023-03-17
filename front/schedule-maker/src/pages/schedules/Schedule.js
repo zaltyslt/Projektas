078c82829c960568c2db7e3f79c68bc49b032800
sandra jonikaite <sandrajonikaite@gmail.com>
2023-03-17 10:04:08 +0200

    schedule lessons view

--- front/schedule-maker/src/pages/schedules/Schedule.js
+++ front/schedule-maker/src/pages/schedules/Schedule.js
@@ -6,6 +6,7 @@ import listPlugin from "@fullcalendar/list";
 import allLocales from "@fullcalendar/core/locales-all";
 import FullCalendar from "@fullcalendar/react";
 import { Link, useParams } from "react-router-dom";
+import "./ViewSchedule.css";
 
 export function Schedule() {
   const [weekendsVisible, setWeekendsVisible] = useState(true);
@@ -13,33 +14,38 @@ export function Schedule() {
   const params = useParams();
 
   useEffect(() => {
-      fetch(`api/v1/schedules/${params.id}/lessons`)
-        .then(response => response.json())
-        .then(data => setSchedule(data))
-        .catch(error => console.error(error));
-  }, [params.id]
-  );
+    fetch(`api/v1/schedules/${params.id}/lessons`)
+      .then((response) => response.json())
+      .then((data) => setSchedule(data))
+      .catch((error) => console.error(error));
+  }, [params.id]);
 
-  console.log(schedule)
+  console.log(schedule);
 
-  const events = schedule.map(schedule => ({
-    title: `${schedule.subject.name}
-      <br />
-      ${schedule.teacher.lName} ${schedule.teacher.fName}
-      <br />
-      ${schedule.classroom.classroomName}
-      <br />
-      ${schedule.lessonStart} - ${schedule.lessonEnd}`,
+  const events = schedule.map((schedule) => ({
+    title: `
+    ${schedule.lessonStart} - ${schedule.lessonEnd} <br />
+    ${schedule.subject.name} <br />
+    ${schedule.teacher.lName} ${schedule.teacher.fName} <br />
+    ${schedule.classroom.classroomName} 
+      `,
     start: schedule.date,
     allDay: true,
   }));
-  
+
   const renderEventContent = (eventInfo) => (
     <>
       <b>{eventInfo.timeText}</b>
-      <div dangerouslySetInnerHTML={{ __html: eventInfo.event.title }} />
+      <div style={{ paddingLeft: "10px", fontSize: '20px', fontFamily: 'Arial, sans-serif', backgroundColor: "lightsteelblue", color: "black" }} dangerouslySetInnerHTML={{ __html: eventInfo.event.title }} />
     </>
   );
+
+  // const renderEventContent = (eventInfo) => (
+  //   <>
+  //     <b>{eventInfo.timeText}</b>
+  //     <div dangerouslySetInnerHTML={{ __html: eventInfo.event.title }} />
+  //   </>
+  // );
 
   return (
     <div className="maincontainer">
@@ -53,11 +59,23 @@ export function Schedule() {
           headerToolbar={{
             left: "prev,next today",
             center: "title",
-            right: "dayGridMonth,dayGridWeek,dayGridDay"
+            right: "dayGridMonth,dayGridWeek,dayGridDay",
           }}
           events={events}
           weekends={false}
           eventContent={renderEventContent}
+          dayHeaderFormat={{
+            weekday: "long", // or 'short'
+          }}
+          dayHeaderClassNames="fc-day-header-black"
+          dayHeaderContent={(args) => (
+            <div className="fc-day-header">{args.text}</div>
+          )}
+          dayCellContent={(args) => (
+            <div className="fc-day-number">
+              {args.dayNumberText}
+            </div>
+          )}
         />
       </div>
     </div>
