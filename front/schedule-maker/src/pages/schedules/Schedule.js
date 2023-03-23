7e06468333d604fc4c27db3ef3a971c6e76f9c52
sandra jonikaite <sandrajonikaite@gmail.com>
2023-03-23 20:01:02 +0200

    add lessons colors

--- front/schedule-maker/src/pages/schedules/Schedule.js
+++ front/schedule-maker/src/pages/schedules/Schedule.js
@@ -10,11 +10,14 @@ import "./ViewSchedule.css";
 import { Stack } from "@mui/system";
 import { Button, Grid } from "@mui/material";
 import adaptivePlugin from "@fullcalendar/adaptive";
+import { render } from "preact/compat";
 
 export function Schedule() {
   const [weekendsVisible, setWeekendsVisible] = useState(true);
   const [schedule, setSchedule] = useState([]);
   const params = useParams();
+
+  const colors = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800', '#FF5722', '#795548', '#9E9E9E', '#607D8B'];
 
   useEffect(() => {
     fetch(`api/v1/schedules/${params.id}/lessons`)
@@ -23,23 +26,29 @@ export function Schedule() {
       .catch((error) => console.error(error));
   }, [params.id]);
 
-  const events = schedule.map((schedule) => ({
-    title: `<b>${schedule.subject.name}</b>
-      <br />
-      ${schedule.lessonStart} - ${schedule.lessonEnd}
-      <br /> 
-      ${schedule.teacher ? schedule.teacher.lName : ""} ${
-      schedule.teacher ? schedule.teacher.fName : "nepasirinktas"
-    }
-      <br />
-      ${
-        schedule.online ? "Nuotolinė pamoka" : schedule.classroom.classroomName
-      }<br />
-      `,
-    start: schedule.date,
-    allDay: true,
-    url: `http://localhost:3000/schedule-maker#/schedules/edit-lesson/${schedule.id}`,
-  }));
+  const events = schedule.map((schedule) => {
+    const color = colors[schedule.subject.id % colors.length];
+
+    return {
+      title: `<b>${schedule.subject.name}</b>
+        <br />
+        ${schedule.lessonStart} - ${schedule.lessonEnd}
+        <br /> 
+        ${schedule.teacher ? schedule.teacher.lName : ""} ${
+        schedule.teacher ? schedule.teacher.fName : "nepasirinktas"
+      }
+        <br />
+        ${
+          schedule.online ? "Nuotolinė pamoka" : schedule.classroom.classroomName
+        }<br />
+        `,
+      start: schedule.date,
+      allDay: true,
+      url: `http://localhost:3000/schedule-maker#/schedules/edit-lesson/${schedule.id}`,
+      // Set the event color to the random color
+      color: color,
+    };
+  });
 
   const renderEventContent = (eventInfo) => (
     <>
