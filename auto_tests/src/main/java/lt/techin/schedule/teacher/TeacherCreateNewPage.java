ecfac8a8f2aef18495eeb5ba066649def86cfd56
Rugile Kicaite <kicaiterugile@gmail.com>
2023-03-28 18:10:10 +0300

    Updating TeacherCreateNew auto test

--- auto_tests/src/main/java/lt/techin/schedule/teacher/TeacherCreateNewPage.java
+++ auto_tests/src/main/java/lt/techin/schedule/teacher/TeacherCreateNewPage.java
@@ -1,42 +1,107 @@
 package lt.techin.schedule.teacher;
 
 import lt.techin.schedule.AbstractPage;
+import lt.techin.schedule.utils.SelectOptionElement;
+import lt.techin.schedule.utils.WaitUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class TeacherCreateNewPage extends AbstractPage {
+
+    private SelectOptionElement selectSiftOptions;
+    private SelectOptionElement selectSubjectOptions;
     @FindBy(css = "#fName")
     private WebElement teacherNameInput;
 
-    @FindBy (css = "#lName-label")
+    @FindBy(css = "#lName")
     private WebElement teacherSurnameInput;
 
-    @FindBy (css = "#contacts.phoneNumber-label")
+    @FindBy(xpath = "//*[@id=\"contacts.phoneNumber\"]")
     private WebElement teacherPhoneInput;
 
-    @FindBy (css = "#contacts.teamsName")
+    @FindBy(xpath ="//*[@id=\"contacts.directEmail\"]" )
+    private WebElement teacherEmailInput;
+
+    @FindBy(xpath = "//*[@id=\"contacts.teamsName\"]")
     private WebElement teacherTeamsNameInput;
 
-    @FindBy (css = "#contacts.teamsEmail-label")
+    @FindBy(xpath = "//*[@id=\"contacts.teamsEmail\"]")
     private WebElement teacherTeamsEmailInput;
 
-    @FindBy (css = "#workHoursPerWeek")
+    @FindBy(css = "#workHoursPerWeek")
     private WebElement teacherWorkHoursInput;
 
+    @FindBy(xpath = "//*[@id=\"teacher.selectedShift\"]")
+    private WebElement selectSift;
+
+    @FindBy(css = "#add-subject-create-teacher")
+    private WebElement selectSubject;
+
+    @FindBy(css = "#save-button-create-teacher")
+    private WebElement clickOnSaveButton;
+
+    @FindBy(css = "#back-button-create-teacher")
+    private WebElement clickOnBackButton;
+
+    @FindBy(css = ".MuiAlert-message")
+    private WebElement alertMessage;
 
 
-
-        public TeacherCreateNewPage(WebDriver driver) {
+    public TeacherCreateNewPage(WebDriver driver) {
         super(driver);
     }
 
-    public void teacherNameInput(String teacherName)
-    { teacherNameInput.sendKeys(teacherName);}
-    public void teacherSurnameInput(String teacherSurname)
-    { teacherSurnameInput.sendKeys(teacherSurname);}
-    public void teacherPhoneNumberInput(String phoneNumber)
-    { teacherPhoneInput.sendKeys(phoneNumber);}
+    public void setFirstName(String firstName) {
+        teacherNameInput.sendKeys(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        teacherSurnameInput.sendKeys(lastName);
+    }
+
+    public void setPhone(String phone) {
+        teacherPhoneInput.sendKeys(phone);
+            }
+    public void setEmail(String email) {
+        teacherEmailInput.sendKeys(email);
+    }
+
+
+    public void setTeamsName(String teamsName) {
+        teacherTeamsNameInput.sendKeys(teamsName);
+    }
+
+    public void setTeamsEmail(String teamsEmail) {
+        teacherTeamsEmailInput.sendKeys(teamsEmail);
+    }
+
+    public void setHours(Integer hours) {
+        teacherWorkHoursInput.sendKeys(String.valueOf(hours));
+    }
+
+    public List<String> getSelectSiftOptions() {
+        selectSiftOptions = new SelectOptionElement(selectSift, driver);
+        return selectSiftOptions.getOptions();
+    }
+
+    public void selectShiftOption(int optionIndex) {
+        selectSiftOptions.selectByIndex(optionIndex);
+    }
+
+    public void clickOnSaveButton() {
+        clickOnSaveButton.click();
+    }
+
+    public void clickOnBackButton() {
+        clickOnBackButton.click();
+    }
+
+    public WebElement getAlertMessage() {
+        return WaitUtils.getVisibleWithWait(alertMessage, driver);
+    }
 
 
 }
--- auto_tests/src/test/java/lt/techin/schedule/TeacherPageTest.java
+++ auto_tests/src/test/java/lt/techin/schedule/TeacherPageTest.java
@@ -1,12 +1,17 @@
 package lt.techin.schedule;
 
+import lt.techin.schedule.teacher.TeacherCreateNewPage;
 import lt.techin.schedule.teacher.TeacherListPage;
+import lt.techin.schedule.utils.WaitUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.List;
 
 public class TeacherPageTest extends BaseTest {
     HomePage homePage;
     TeacherListPage teacherListPage;
+    TeacherCreateNewPage teacherCreateNewPage;
 
     @ParameterizedTest
     @CsvFileSource(resources = "/teacher_new.csv")
@@ -16,9 +21,24 @@ public class TeacherPageTest extends BaseTest {
 
         homePage = new HomePage(driver);
         teacherListPage = new TeacherListPage(driver);
+        teacherCreateNewPage = new TeacherCreateNewPage(driver);
 
         homePage.navigateToMokytojai();
         teacherListPage.selectCreateNewTeacherButton();
+        WaitUtils.waitPageToLoad(driver);
+
+        teacherCreateNewPage.setFirstName(firstName);
+        teacherCreateNewPage.setLastName(lastName);
+        teacherCreateNewPage.setPhone(phone);
+        teacherCreateNewPage.setEmail(email);
+        teacherCreateNewPage.setTeamsName(teamsName);
+        teacherCreateNewPage.setTeamsEmail(teamsEmail);
+        teacherCreateNewPage.setHours(40);
+        List<String> siftOptions = teacherCreateNewPage.getSelectSiftOptions();
+        teacherCreateNewPage.selectShiftOption(1);
+
+
+
 
         System.out.println(driver.getCurrentUrl());
 
