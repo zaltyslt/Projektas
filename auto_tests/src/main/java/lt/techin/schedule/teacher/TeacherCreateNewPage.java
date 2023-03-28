d5e82b7e9afcf4045644429eaf7e8f56d0b988fd
Rugile Kicaite <kicaiterugile@gmail.com>
2023-03-28 10:29:58 +0300

    Creating Teacher auto tests

---
+++ auto_tests/src/main/java/lt/techin/schedule/teacher/TeacherCreateNewPage.java
@@ -0,0 +1,10 @@
+package lt.techin.schedule.teacher;
+
+import lt.techin.schedule.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+public class TeacherCreateNewPage extends AbstractPage {
+    public TeacherCreateNewPage(WebDriver driver) {
+        super(driver);
+    }
+}
