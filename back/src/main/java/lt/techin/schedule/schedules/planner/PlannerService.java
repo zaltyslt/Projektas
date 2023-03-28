a0c1e5071e15a8b25ade1267b60dcf94e35abebc
Karolina Čižaitė <61018680+kcizaite@users.noreply.github.com>
2023-03-28 18:59:30 +0300

    Commit

--- back/src/main/java/lt/techin/schedule/schedules/planner/PlannerService.java
+++ back/src/main/java/lt/techin/schedule/schedules/planner/PlannerService.java
@@ -263,4 +263,6 @@ public class PlannerService {
             throw new ValidationException("Dalykas neturi suplanuotų valandų.", "Subject", "Subject does not have planned hours in schedule", subjectId.toString());
         }
     }
+
+    
 }
