26d4175dbe2f32326a509cc15f7232bdc275a33d
Karolina Čižaitė <61018680+kcizaite@users.noreply.github.com>
2023-03-29 23:51:14 +0300

    Update sql data and fixed menu

--- back/src/main/java/lt/techin/schedule/group/Group.java
+++ back/src/main/java/lt/techin/schedule/group/Group.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
-@Table(name = "ClassGroup")
+@Table(name = "someThing")
 public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
