a3f5f1a6d7e835316b3efc9d86028c986e9de3e9
sandra jonikaite <sandrajonikaite@gmail.com>
2023-03-29 15:32:51 +0300

    add buttons id

--- front/schedule-maker/src/pages/subjects/ViewSubject.js
+++ front/schedule-maker/src/pages/subjects/ViewSubject.js
@@ -65,11 +65,15 @@ export function ViewSubject() {
           <Grid item sm={12}>
             <Stack direction="row" spacing={2}>
               <Link to={"/subjects/edit/" + subject.id}>
-                <Button variant="contained">Redaguoti</Button>
+                <Button id="edit-button-view-subject" variant="contained">
+                  Redaguoti
+                </Button>
               </Link>
 
               <Link to="/subjects">
-                <Button variant="contained">Grįžti</Button>
+                <Button id="back-button-view-subject" variant="contained">
+                  Grįžti
+                </Button>
               </Link>
             </Stack>
           </Grid>
